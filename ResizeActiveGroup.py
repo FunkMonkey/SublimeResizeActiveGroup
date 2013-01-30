26d8b6c515f75ffe3e12830aead3df527f165ed1
Andre Selmanagic <andre.selmanagic+sublimeresize@gmail.com>
2013-01-30 20:18:46 +0100

    started 3 column / row layout totally unstable and has errors

--- ResizeActiveGroup.py
+++ ResizeActiveGroup.py
@@ -1,52 +1,82 @@
 import sublime_plugin
 from copy import deepcopy
 
-
 class ResizeActiveGroup(sublime_plugin.EventListener):
 
+    def __init__(self):
+        self.currentLayoutID = ""
+        self.cellOrder = []
+        #self.maxItemsInQueue = 0
+        
     # based on the active group we calculate the cols/rows array
     def get_big(self, arr, activeGroup):
         return [arr[0], arr[1] if (activeGroup == 1) ^ (arr[1] > 0.5) else 1 - arr[1], arr[2]]
+    
+    # initialize all necessary layout information
+    def init_layout_info(self, currentLayout, activeGroup):
+        
+        # handling change of layouts
+        currLayoutID = "" + len(currentLayout["cells"]) + "-" + len(currentLayout["cols"]) + "-" + len(currentLayout["rows"])
+        if(self.currentLayoutID != currLayoutID):
+            self.cellOrder = []
+            self.currentLayoutID = currLayoutID
+            #self.maxItemsInQueue = len(currentLayout["cells"])
+        
+        # setting up the order
+        try:
+            self.cellOrder.remove(activeGroup)
+        except ValueError:
+            pass
+        
+        self.cellOrder.append(activeGroup)
+    
+    def get_size_by_order(self, arr)
+        pass
 
     # called when a different view is activated
     def on_activated(self, view):
         
         window = view.window()
         if window:
+            currentLayout = window.get_layout()
             activeGroup = window.active_group()
-            oldLayout = window.get_layout()
-            newLayout = deepcopy(oldLayout)
-
+            self.init_layout_info(currentLayout, activeGroup)
+            
+            newLayout = deepcopy(currentLayout)
             
             # 2 cells
-            if len(oldLayout["cells"]) == 2:
+            if len(currentLayout["cells"]) == 2:
                 # Columns: 2
-                if len(oldLayout["rows"]) == 2:
-                    newLayout["cols"] = self.get_big(oldLayout["cols"], activeGroup)
+                if len(currentLayout["rows"]) == 2:
+                    newLayout["cols"] = self.get_big(currentLayout["cols"], activeGroup)
                 # Rows: 2
                 else:
-                    newLayout["rows"] = self.get_big(oldLayout["rows"], activeGroup)
+                    newLayout["rows"] = self.get_big(currentLayout["rows"], activeGroup)
 
             # 3 cells
-            elif len(oldLayout["cells"]) == 3:
-                pass
+            elif len(currentLayout["cells"]) == 3:
+                if len(currentLayout["rows"] == 4)
+                    pass
+                else
+                    pass
 
             # 4 cells
-            elif len(oldLayout["cells"]) == 4:
+            elif len(currentLayout["cells"]) == 4:
                 # Grid: 4
-                if len(oldLayout["rows"]) == 3 and len(oldLayout["cols"]) == 3:
+                if len(currentLayout["rows"]) == 3 and len(currentLayout["cols"]) == 3:
                     if activeGroup == 0:
-                        newLayout["cols"] = self.get_big(oldLayout["cols"], activeGroup)
-                        newLayout["rows"] = self.get_big(oldLayout["rows"], activeGroup)
+                        newLayout["cols"] = self.get_big(currentLayout["cols"], activeGroup)
+                        newLayout["rows"] = self.get_big(currentLayout["rows"], activeGroup)
                     elif activeGroup == 1:
-                        newLayout["cols"] = self.get_big(oldLayout["cols"], activeGroup)
-                        newLayout["rows"] = self.get_big(oldLayout["rows"], activeGroup - 1)
+                        newLayout["cols"] = self.get_big(currentLayout["cols"], activeGroup)
+                        newLayout["rows"] = self.get_big(currentLayout["rows"], activeGroup - 1)
                     elif activeGroup == 2:
-                        newLayout["cols"] = self.get_big(oldLayout["cols"], activeGroup - 2)
-                        newLayout["rows"] = self.get_big(oldLayout["rows"], activeGroup - 1)
+                        newLayout["cols"] = self.get_big(currentLayout["cols"], activeGroup - 2)
+                        newLayout["rows"] = self.get_big(currentLayout["rows"], activeGroup - 1)
                     elif activeGroup == 3:
-                        newLayout["cols"] = self.get_big(oldLayout["cols"], activeGroup - 2)
-                        newLayout["rows"] = self.get_big(oldLayout["rows"], activeGroup - 2)
+                        newLayout["cols"] = self.get_big(currentLayout["cols"], activeGroup - 2)
+                        newLayout["rows"] = self.get_big(currentLayout["rows"], activeGroup - 2)
 
-            if oldLayout != newLayout:
+            if currentLayout != newLayout:
                 window.set_layout(newLayout)
+            
